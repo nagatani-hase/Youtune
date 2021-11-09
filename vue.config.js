60774bc4ee7bf193bb83e09fc1d19527ff8102a2
長谷侑哉 <y-nagatani@hokuryodenko.co.jp>
2021-11-09 14:09:31 +0900

    windows用ビルド設定対応

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,//これがないと "__dirname" がつかえないらしい
+      builderOptions: {
+        win: {
+          icon: 'src/assets/icon.png', //exeをビルドした時のアイコンを指定
+          target: [
+            {
+              target: 'portable', 
+              arch: ['x64'], 
+            },
+          ],
+        },
+      },
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
